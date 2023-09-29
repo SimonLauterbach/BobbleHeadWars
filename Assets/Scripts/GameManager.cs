c6a888b431151b186943d505bee73538dc76384c
SimonLauterbach <simon.lauterbach02@gmail.com>
2023-09-29 08:52:05 -0400

    Chapter 5 - Created Game Manager & added SpaceMarine, Alien, and Spawnpoints

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject[] spawnPoints;
+    public GameObject alien;
+
+    public int maxAliensOnScreen;
+    public int totalAliens;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    public int aliensPerSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
