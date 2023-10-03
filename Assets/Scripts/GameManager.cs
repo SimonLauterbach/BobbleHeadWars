070d1a9e4c5045eb5cafb24e6f72877b91b9d5d0
SimonLauterbach <simon.lauterbach02@gmail.com>
2023-10-03 15:22:19 -0400

    Chapter 5 - Added comments to the GameManager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,10 +19,6 @@ public class GameManager : MonoBehaviour
     private float generatedSpawnTime = 0;
     private float currentSpawnTime = 0;
 
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -32,15 +28,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //currentSpawnTime time passed since last update cell
         currentSpawnTime += Time.deltaTime;
 
+        //Condition to gnereate a new wave of Aliens
         if (currentSpawnTime > generatedSpawnTime)
         {
+            //Resets the timer after a spawn occurs
             currentSpawnTime = 0;
         }
 
+        //Spawn-Time randomizer
         generatedSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
 
+        //Ensures number of Aliens is within limits
         if (aliensPerSpawn > 0 && aliensOnScreen < totalAliens)
         {
             List<int> previousSpawnLocations = new List<int>();
@@ -48,54 +49,61 @@ public class GameManager : MonoBehaviour
 
         if (aliensPerSpawn > 0 && aliensOnScreen < totalAliens)
         {
+            //This list keeps track of where you have already spawned Aliens
             List<int> previousSpawnLocations = new List<int>();
 
 
-
+            //Limits number of Aliens to number of Spawnpoints
             if (aliensPerSpawn > spawnPoints.Length)
             {
                 aliensPerSpawn = spawnPoints.Length - 1;
-
-
-
             }
+
+            //Preventative code to make sure you do not spawn more Aliens than you've configured
 
             aliensPerSpawn = (aliensPerSpawn > totalAliens) ?
                               aliensPerSpawn - totalAliens : aliensPerSpawn;
 
 
-
+            //This code loops for each Alien spawned
             for (int i = 0; i < aliensPerSpawn; i++)
             {
                 if (aliensOnScreen < maxAliensOnScreen)
                 {
+                    //Keeps track of number of Aliens
                     aliensOnScreen += 1;
 
-
-
+                    //Value of -1 means no index has been assigned or found for the spawnpoint
                     int spawnPoint = -1;
 
-
-
+                    //While look keeps looking for a spawning point (index) that has not been used yet
                     while (spawnPoint == -1)
                     {
+                        //create random index of List(array) between 0 and number of spawnpoints
                         int randomNumber = Random.Range(0, spawnPoints.Length - 1);
-
+                        //check to see if random spawnpoint has not already been used
                         if (!previousSpawnLocations.Contains(randomNumber))
                         {
+                            //add this random number to the list
                             previousSpawnLocations.Add(randomNumber);
+                            //use this random number for the spawn location
                             spawnPoint = randomNumber;
                         }
                     }
 
+                    //Actual point(label) on arena to spawn next Alien
                     GameObject spawnLocation = spawnPoints[spawnPoint];
 
+                    //Code to actually create a new Alien from a Prefab
                     GameObject newAlien = Instantiate(alien) as GameObject;
 
+                    //Position the new Alien to the random unused spawn point
                     newAlien.transform.position = spawnLocation.transform.position;
 
+                    //Get the Alien code from the new Alien spawned
                     Alien alienScript = newAlien.GetComponent<Alien>();
 
+                    //Set the new Alien to target the player is currently
                     alienScript.target = player.transform;
 
                     Vector3 targetRotation = new Vector3(player.transform.position.x,
